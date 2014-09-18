bb6d9268ef95cf271f4c5a1ecd898ff412154e7a
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2014-09-18 15:39:43 +1000

    Rejigged the JS and partial to allow for multiple uses on one page by wrapping the partial in a generic wrapper, and the JS does the setup for each wrapper on the page.

--- app/assets/javascripts/active_admin/geocomplete.js
+++ app/assets/javascripts/active_admin/geocomplete.js
@@ -1,34 +1,37 @@
 $(document).ready(function(){
 
-  $find_field = $(".find_location_input");
+  $(".aageocomplete_wrapper").each(function() {
+    $wrapper = $(this);
+    $find_field = $(".find_location_input", $wrapper);
+    $map_canvas = $(".map_canvas", $wrapper);
 
-  //setup
-  $find_field.geocomplete({
-    map: ".map_canvas",
-    markerOptions: {
-      draggable: true
-    },
-    details: "form ol",
-    detailsAttribute: "data-geo"
+    //setup
+    $find_field.geocomplete({
+      map: $map_canvas,
+      markerOptions: {
+        draggable: true
+      },
+      details: $wrapper,
+      detailsAttribute: "data-geo"
+    });
+
+    /////////////////////////////////////////////////////////
+    $find_field.bind("geocode:result", function(event, result){
+      // console.log("result");
+      // console.log(result.address_components);
+      var full_address = $("input[data-geo=formatted_address]", $wrapper).val();
+    });
+
+    /////////////////////////////////////////////////////////
+    $find_field.bind("geocode:dragged", function(event, latLng){
+      $("input[data-geo=lat]", $wrapper).val(latLng.lat());
+      $("input[data-geo=lng]", $wrapper).val(latLng.lng());
+      // $find_field.geocomplete("find", latLng.lat() + ',' + latLng.lng()); // so we get address updated on drag as well
+    });
+    /////////////////////////////////////////////////////////
+
+    // let's use lat/long to display an existing location
+    var location = $("input[data-geo=lat]", $wrapper).val() + ',' + $("input[data-geo=lng]", $wrapper).val();
+    $find_field.geocomplete("find", location);
   });
-
-  /////////////////////////////////////////////////////////
-  $find_field.bind("geocode:result", function(event, result){
-    // console.log("result");
-    // console.log(result.address_components);
-    var full_address = $("input[data-geo=formatted_address]").val();
-  });
-
-  /////////////////////////////////////////////////////////
-  $find_field.bind("geocode:dragged", function(event, latLng){
-    $("input[data-geo=lat]").val(latLng.lat());
-    $("input[data-geo=lng]").val(latLng.lng());
-    // $find_field.geocomplete("find", latLng.lat() + ',' + latLng.lng()); // so we get address updated on drag as well 
-  });
-  /////////////////////////////////////////////////////////
-
-  // let's use lat/long to display an existing location
-  var location = $("input[data-geo=lat]").val() + ',' + $("input[data-geo=lng]").val();
-  $find_field.geocomplete("find", location);
-
 });
